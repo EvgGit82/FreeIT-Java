226bc16c2ccc83164abf0a9628fb0ea30f6c4d04
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2021-03-28 15:55:43 +0300

    Fix starship names list

--- src/lesson_5/com/interfacetasks/spaceport/SpacePortRunner.java
+++ src/lesson_5/com/interfacetasks/spaceport/SpacePortRunner.java
@@ -40,10 +40,8 @@ public class SpacePortRunner {
 
         List<IStart> spaceships = new ArrayList<>(){{
             add(new Shuttle("Discovery"));
-            add(new SpaceX("Of Course I Still Love You"));
             add(new Shuttle("Columbia"));
             add(new Soyuz("TMA"));
-            add(new SpaceX("Just Read the Instructions"));
             add(new Progress("M1"));
             add(new Shuttle("Challenger"));
             add(new SpaceX("GO Ms. Tree"));
