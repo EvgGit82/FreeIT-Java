e2d253b967dedf4ab262773fe41c2e66fdf0dd93
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2021-04-11 21:07:30 +0300

    Add lesson 8 main tasks

---
+++ src/lesson_8/Task34/NumbersFinderRunner.java
@@ -0,0 +1,23 @@
+package lesson_8.Task34;
+
+/*
+Имеется файл с текстом, в котором присутствуют числа. Найти все числа, распечатать, посчитать сумму, убрать все
+повторяющиеся числа и снова распечатать.
+*/
+
+public class NumbersFinderRunner {
+
+    private final static String FILE_PATH = "src/lesson_8/Task34/sourceText";
+
+    public static void main(String[] args) {
+
+        NumbersFinder numFinder = new NumbersFinder();
+
+        numFinder.readFile(FILE_PATH);
+
+        numFinder.getNumbersAndNonRepeatingNumbers();
+
+        numFinder.printResults();
+
+    }
+}
---
+++ src/lesson_8/Task36/FileListRunner.java
@@ -0,0 +1,23 @@
+package lesson_8.Task36;
+
+/*
+Вывести список файлов и каталогов выбранного каталога на диске. Файлы и каталоги должны быть распечатаны отдельно.
+*/
+
+import java.io.File;
+
+public class FileListRunner {
+
+    public static final String PATH = ""; //Indicate path to the folder here
+
+    public static void main(String[] args) {
+
+        FilesLister filesLister = new FilesLister();
+
+        File file = new File(PATH);
+
+        filesLister.getFilesAndFolders(file);
+
+        filesLister.printFilesAndFolders();
+    }
+}
