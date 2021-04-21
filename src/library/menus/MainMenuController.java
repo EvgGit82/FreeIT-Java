35edae854b6d108815b46823e976927ba65ff5bd
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2021-04-21 23:28:07 +0300

    Delete Factory class

--- src/library/menus/MainMenuController.java
+++ src/library/menus/MainMenuController.java
@@ -1,16 +1,30 @@
 package library.menus;
 
 import library.db_commands.PrintBookList;
+import library.menus.submenu_types.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainMenuController {
 
+    private Map<Integer, IMenu> subMenus = new HashMap<>();
+
+    public MainMenuController() {
+        subMenus.put(1, new SortingMenu());
+        subMenus.put(2, new AddBookMenu());
+        subMenus.put(3, new DeleteBookMenu());
+        subMenus.put(4, new EditBookMenu());
+        subMenus.put(0, new QuitMenu());
+    }
+    
     public void runSubmenu (int option) {
 
         if (option == 1) {
-            new MenuFactory().createSubMenu(option).runMenu();
+            subMenus.get(option).runMenu();
             new PrintBookList().execute();
         } else {
-            new MenuFactory().createSubMenu(option).runMenu();
+            subMenus.get(option).runMenu();
             System.out.println();
         }
     }
