a9e66a010ec84b809f1e081a0765dcafa4595e01
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2021-04-22 00:23:01 +0300

    Make Genre representation abstract

--- src/library/utils/CommonProperties.java
+++ src/library/utils/CommonProperties.java
@@ -1,10 +1,28 @@
 package library.utils;
 
 import library.entity_classes.Genre;
+import library.enums.GenreName;
+
+
+import java.util.Iterator;
+import java.util.Map;
 
 public class CommonProperties {
 
     public static final int NUMBER_OF_GENRES = new Genre(0).getGenresList().size()-1;
     public static final int LIBRARY_CAPACITY = 99;
+    public static String GENRES_REPRESENTATION = getGenresRepresentation();
 
+
+    private static String getGenresRepresentation (){
+        String result = "";
+        Iterator iterator = new Genre(0).getGenresList().entrySet().iterator();
+
+        while (iterator.hasNext()) {
+            Map.Entry<Iterator, GenreName> pair = ( Map.Entry<Iterator, GenreName>) iterator.next();
+            result += pair.getKey() + "-" + pair.getValue() + ", ";
+
+        }
+            return "\"" + result.substring(0, result.length()-2) + "\"";
+    }
 }
--- src/library/utils/GetGenreNumber.java
+++ src/library/utils/GetGenreNumber.java
@@ -5,8 +5,7 @@ public class GetGenreNumber {
     public int execute () {
         int genre;
         do {
-            System.out.println("Enter Genre using number (1-Adventure, 2-Biography, 3-Fantasy, 4-Historical fiction, " +
-                    "5-Horror, 6-Mystery, 7-Religion, 8-Romance, 9-Science fiction, 10-Thriller, 0-No genre specified):");
+            System.out.println("Enter Genre using number (" + CommonProperties.GENRES_REPRESENTATION + "):");
             genre = new GetNumberFromScanner().execute();
         } while (genre < 0 || genre > CommonProperties.NUMBER_OF_GENRES);
 
