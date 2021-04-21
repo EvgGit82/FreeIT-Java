1a43fffbee3a04b12eb2882bd2a5700580751058
Evgeniy Tsarkov <evgeniy_tsarkov@epam.com>
2021-04-21 23:23:34 +0300

    Change subMenu facory initialization

--- src/library/menus/MenuFactory.java
+++ src/library/menus/MenuFactory.java
@@ -7,14 +7,17 @@ import java.util.Map;
 
 public class MenuFactory {
 
-    public IMenu createSubMenu(int option) {
-        Map <Integer, IMenu> subMenus = new HashMap<>();
+    private Map <Integer, IMenu> subMenus = new HashMap<>();
+
+    public MenuFactory() {
         subMenus.put(1, new SortingMenu());
         subMenus.put(2, new AddBookMenu());
         subMenus.put(3, new DeleteBookMenu());
         subMenus.put(4, new EditBookMenu());
         subMenus.put(0, new QuitMenu());
+    }
 
+    public IMenu createSubMenu(int option) {
         return subMenus.get(option);
     }
 }
